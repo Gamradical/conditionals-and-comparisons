755cb5a2e99afe8323cace5c4b3e979a92234b13
Maureen_g <gamradicalsix@gmail.com>
2023-03-08 14:47:39 -0600

    Initial commit learning JS conditionals and comparisons

---
+++ script.js
@@ -0,0 +1,12 @@
+var stringTwelve = "12";
+var numberTwelve = 12;
+
+if (stringTwelve === numberTwelve) {
+  console.log("These values are equal in value and type.");
+}
+else if (stringTwelve == numberTwelve) {
+  console.log("These two values are equal.");
+}
+else {
+  console.log("These numbers are not equal in value or type somehow.");
+}
